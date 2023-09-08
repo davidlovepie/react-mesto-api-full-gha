906e0ce2557bb97438840c0abfcf44837545b71f
Devid Fan <cptdavid@yandex.ru>
2023-09-08 10:53:30 +0300

    Проектная 15 Проверка 1

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -16,7 +16,7 @@ function Main({
 }) {
 
   const currentUser = useContext(CurrentUserContext);
-
+console.log('currentUser', currentUser);
   return (
     <main className="content">
       <section className="profile">
