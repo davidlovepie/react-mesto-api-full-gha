3f3ff1a57e135ffc441b8a731b93effccaa6163d
Devid Fan <cptdavid@yandex.ru>
2023-09-08 12:14:29 +0300

    Проектная 15 Проверка 1

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -15,7 +15,7 @@ function Card({
 }) {
   const currentUser = useContext(CurrentUserContext);
   const isOwn = card.owner === currentUser._id;
-  const isLiked = card.likes.some((i) => i._id === currentUser._id);
+  const isLiked = card.likes.some((i) => i === currentUser._id);
   const cardLikeButtonClassName = `elements__like-button ${
     isLiked && "elements__like-button_active"
   }`;
